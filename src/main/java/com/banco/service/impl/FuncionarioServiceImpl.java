043d8bb1eefd82b43878fbe05bbbd6075ef1ea2b
Rafael Cunha <rafaelpp9@gmail.com>
2023-10-20 10:49:56 +0100

    Refactor: FuncionarioServiceImpl

--- src/main/java/com/banco/service/impl/FuncionarioServiceImpl.java
+++ src/main/java/com/banco/service/impl/FuncionarioServiceImpl.java
@@ -3,53 +3,60 @@ package com.banco.service.impl;
 import java.util.List;
 
 import com.banco.model.FuncionarioAbstrato;
+import com.banco.repository.ClienteDAO;
+import com.banco.repository.FuncionarioDAO;
+import com.banco.repository.impl.FuncionarioDAOImpl;
 import com.banco.service.FuncionarioService;
 
 public class FuncionarioServiceImpl implements FuncionarioService {
+	
+	private FuncionarioDAO repository;
 
 	@Override
 	public void create(FuncionarioAbstrato obj) {
+		repository = new FuncionarioDAOImpl();
 		
 	}
 
 	@Override
 	public List<FuncionarioAbstrato> readAll() {
-		return null;
+		return repository.readAll();
 	}
 
 	@Override
 	public FuncionarioAbstrato update(String id) {
-		return null;
+		return repository.update(id);
 	}
 
 	@Override
 	public void delete(String id) {
+		repository.delete(id);
 		
 	}
 
 	@Override
 	public int readIndex(String id) {
-		return 0;
+		return repository.readIndex(id);
 	}
 
 	@Override
 	public FuncionarioAbstrato updateName(String id, String name) {
-		return null;
+		return repository.updateName(id, name);
 	}
 
 	@Override
 	public FuncionarioAbstrato update(String id, FuncionarioAbstrato obj) {
-		return null;
+		return repository.update(id, obj);
 	}
 
 	@Override
 	public FuncionarioAbstrato read(String id) {
-		return null;
+		return repository.read(id);
 	}
 
 	@Override
 	public FuncionarioAbstrato idOrdenado(String id) {
-		return null;
+		return repository.idOrdenado(id);
 	}
 
 	
