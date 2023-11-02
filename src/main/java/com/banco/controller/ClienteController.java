fbb26772b254d64ca93532c98ae273b3d689c449
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-02 17:30:46 +0000

    Feat: ClienteController

--- src/main/java/com/banco/controller/ClienteController.java
+++ src/main/java/com/banco/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package com.banco.controller;
 
+import java.util.List;
+
 import com.banco.model.ClienteAbstrato;
 import com.banco.service.ClienteService;
 import com.banco.service.impl.ClienteServiceImpl;
@@ -15,9 +17,21 @@ public class ClienteController {
 	public void create(ClienteAbstrato obj) {
 		service.create(obj);
 	}
+	
+	public ClienteAbstrato update(String id, ClienteAbstrato obj) {
+		return service.update(id, obj);
+	}
 
 	public ClienteAbstrato read(String id) {
 		return service.read(id);
+	}
+	
+	public List<ClienteAbstrato> readAll() {
+		return service.readAll();
+	}
+	
+	public void delete(String id) {
+		service.delete(id);
 	}
 
 }
