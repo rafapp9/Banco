302bf4a4d71e262ff9e4525ba2248781a67d19b7
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-09 10:19:02 +0000

    Feat: added remaining features for ClienteControllerTest

--- src/test/java/com/banco/controller/ClienteControllerTest.java
+++ src/test/java/com/banco/controller/ClienteControllerTest.java
@@ -4,88 +4,94 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.banco.model.ClienteAbstrato;
 import com.banco.model.ClienteStandard;
-import com.banco.service.impl.ClienteServiceImpl;
+import com.banco.service.ClienteService;
 
 public class ClienteControllerTest {
 
-	@Before
+	private static ClienteController service = new ClienteController();
+
 	@Test
 	public void create() {
-		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", 500);
-		ClienteController service = new ClienteController();
+		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
 		service.create(cliente);
 		ClienteAbstrato clienteRead = service.read("123456789");
 		assertEquals("123456789", clienteRead.getId());
 	}
 
-	@Before
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void createNifExist() {
-		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", 500);
-		ClienteAbstrato cliente2 = new ClienteStandard("123456789", "Antonio Cunha", 500);
-		ClienteController service = new ClienteController();
+		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
+		ClienteAbstrato cliente2 = new ClienteStandard("123456789", "Antonio Serra", "Homem", LocalDate.of(1995, 1, 1),
+				500);
 		service.create(cliente);
 		service.create(cliente2);
 		ClienteAbstrato clienteRead = service.read("123456789");
 		assertEquals("123456789", clienteRead.getId());
 	}
 
-	@Before
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void createNifMenor() {
-		ClienteAbstrato cliente = new ClienteStandard("12345678", "Rafael Cunha", 500);
-		ClienteController service = new ClienteController();
+		ClienteAbstrato cliente = new ClienteStandard("12345678", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
 		service.create(cliente);
 		assertEquals(null, service.read("12345678"));
 	}
 
-	@Before
 	@Test
 	public void readAll() {
-		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", 500);
-		ClienteAbstrato cliente1 = new ClienteStandard("987654321", "Antonio Serra", 500);
-		ClienteController service = new ClienteController();
+		ClienteAbstrato cliente = new ClienteStandard("123456712", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
+		ClienteAbstrato cliente1 = new ClienteStandard("987654121", "Antonio Serra", "Homem", LocalDate.of(1995, 1, 1),
+				500);
 		service.create(cliente);
 		service.create(cliente1);
 
 		List<ClienteAbstrato> clientes = service.readAll();
 		assertNotNull(clientes);
-		assertEquals(2, clientes.size());
+		assertEquals(5, clientes.size());
 
 	}
 
-	@Before
 	@Test
 	public void update() {
-	    ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", 500);
-	    ClienteController service = new ClienteController();   
-	    service.create(cliente);
-	    cliente.setNome("Novo Nome");
-	    service.update("123456789", cliente);
-	    ClienteAbstrato clienteRead = service.read("123456789");
-	    assertEquals("Novo Nome", clienteRead.getNome());
-	}  
+		ClienteAbstrato cliente = new ClienteStandard("123456713", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
+		service.create(cliente);
+		cliente.setNome("Novo Nome");
+		service.update("123456713", cliente);
+		ClienteAbstrato clienteRead = service.read("123456713");
+		assertEquals("Novo Nome", clienteRead.getNome());
+	}
 
-
-	@Before
 	@Test
 	public void delete() {
-		ClienteAbstrato cliente = new ClienteStandard("123456789", "Rafael Cunha", 500);
-		ClienteAbstrato cliente2 = new ClienteStandard("923456779", "Antonio Serra", 500);
-		ClienteController service = new ClienteController();
+		ClienteAbstrato cliente = new ClienteStandard("123456743", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22),
+				500);
+		ClienteAbstrato cliente2 = new ClienteStandard("234564321", "Antonio Serra", "Homem", LocalDate.of(1995, 1, 1),
+				500);
 		service.create(cliente);
 		service.create(cliente2);
-		service.delete("923456779");
-		ClienteAbstrato clienteEliminado = service.read("923456779");
+		service.delete("234564321");
+		ClienteAbstrato clienteEliminado = service.read("234564321");
 		assertNull(clienteEliminado);
 
-	} 
+	}
+
+	@AfterClass
+	public static void cleanup() {
+		service = null;
+	}
 
 }
