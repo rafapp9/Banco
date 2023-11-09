f03af7921aa9b4592422a96b681e72aa5be5ffdb
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-09 10:18:01 +0000

    Fix

--- src/main/java/com/banco/study/MainEnum.java
+++ src/main/java/com/banco/study/MainEnum.java
@@ -1,5 +1,7 @@
 package com.banco.study;
 
+import java.time.LocalDate;
+
 import com.banco.model.FuncionarioAbstrato;
 import com.banco.model.FuncionarioGerente;
 import com.banco.repository.FuncionarioDAO;
@@ -8,8 +10,8 @@ import com.banco.repository.impl.FuncionarioDAOImpl;
 public class MainEnum {
 
 	public static void main(String[] args) {
-		FuncionarioAbstrato funcionario1 = new FuncionarioGerente("12356", "Rafael " + EnumTeste.APELIDO.getName(), 250);
-		FuncionarioAbstrato funcionario2 = new FuncionarioGerente("456789", "Antonio " + EnumTeste.APELIDO.getName(), 250);
+		FuncionarioAbstrato funcionario1 = new FuncionarioGerente("12356", "Rafael " + EnumTeste.APELIDO.getName(), "Homem", LocalDate.of(1996, 7, 22), 250);
+		FuncionarioAbstrato funcionario2 = new FuncionarioGerente("456789", "Antonio " + EnumTeste.APELIDO.getName(), "Homem", LocalDate.of(1995, 1, 1), 250);
 
 		FuncionarioDAO repository1 = new FuncionarioDAOImpl();
 
--- src/main/java/com/banco/study/Vetores.java
+++ src/main/java/com/banco/study/Vetores.java
@@ -1,5 +1,7 @@
 package com.banco.study;
 
+import java.time.LocalDate;
+
 import com.banco.model.FuncionarioAbstrato;
 import com.banco.model.FuncionarioGerente;
 import com.banco.repository.FuncionarioDAO;
@@ -8,9 +10,9 @@ import com.banco.repository.impl.FuncionarioDAOImpl;
 public class Vetores {
 
 	public static void main(String[] args) {
-		FuncionarioAbstrato f1 = new FuncionarioGerente("123456789", "Rafael", 500);
-		FuncionarioAbstrato f2 = new FuncionarioGerente("789456123", "Antonio", 300);
-		FuncionarioAbstrato f3 = new FuncionarioGerente("456789123", "teste", 300);
+		FuncionarioAbstrato f1 = new FuncionarioGerente("123456781", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22), 100);
+		FuncionarioAbstrato f2 = new FuncionarioGerente("234567543", "Miguel Cunha", "Homem", LocalDate.of(1970, 10, 12), 500);
+		FuncionarioAbstrato f3 = new FuncionarioGerente("234567543", "André Galante", "Homem", LocalDate.of(2000, 1, 1), 500);
 
 		FuncionarioDAO repository = new FuncionarioDAOImpl();
 
@@ -18,7 +20,7 @@ public class Vetores {
 		repository.create(f2);
 		repository.create(f3);
 
-		FuncionarioAbstrato fUpdate = new FuncionarioGerente("123456789", "Rafael Cunha", 700);
+		FuncionarioAbstrato fUpdate = new FuncionarioGerente("123456781", "Rafael Cunha", "Homem", LocalDate.of(1996, 7, 22), 700);
 
 		repository.update("123456789", fUpdate);
 		
