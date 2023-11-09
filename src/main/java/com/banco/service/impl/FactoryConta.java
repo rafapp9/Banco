cccfedc0eea03de335e5d17d78e15b90c6505ef3
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-09 11:56:24 +0000

    Feat: Exception added for accounts lower than 20.
    Throws new exception

--- src/main/java/com/banco/service/impl/FactoryConta.java
+++ src/main/java/com/banco/service/impl/FactoryConta.java
@@ -2,6 +2,7 @@ package com.banco.service.impl;
 
 import java.util.Date;
 
+import com.banco.exceptions.ContaValidateException;
 import com.banco.model.ContaAbstrata;
 import com.banco.model.ContaCorrente;
 import com.banco.model.ContaDTO;
@@ -9,12 +10,12 @@ import com.banco.model.ContaPremium;
 
 public class FactoryConta {
 
-	public static ContaAbstrata getConta(ContaDTO contaDTO) {
+	public static ContaAbstrata getConta(ContaDTO contaDTO) throws ContaValidateException {
 		if(contaDTO.montanteConta() >= 1500) {
 			return new ContaPremium(contaDTO.id(), contaDTO.numeroDaConta(), new Date(), contaDTO.montanteConta());
 		}else if(contaDTO.montanteConta() < 1500 && contaDTO.montanteConta() > 20) {
 			return new ContaCorrente(contaDTO.id(), contaDTO.numeroDaConta(), new Date(), contaDTO.montanteConta());
 		}
-		return null;
+		throw new ContaValidateException();
 	}
 }
