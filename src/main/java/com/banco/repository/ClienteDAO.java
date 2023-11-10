9fe891f06d677488c8e5fb993bb84d669526e8be
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-10 11:04:15 +0000

    Feat: Super for filterCltByAge

--- src/main/java/com/banco/repository/ClienteDAO.java
+++ src/main/java/com/banco/repository/ClienteDAO.java
@@ -1,7 +1,12 @@
 package com.banco.repository;
 
+import java.util.List;
+
 import com.banco.model.ClienteAbstrato;
+import com.banco.model.FuncionarioAbstrato;
 
 public interface ClienteDAO extends GenericCRUD<ClienteAbstrato, String> {
+
+	List<ClienteAbstrato> filterCltByAge(int age);
 
 }
