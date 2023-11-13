bbf8e76445686e4328d502f43d1ddc9862376f85
Rafael Cunha <rafaelpp9@gmail.com>
2023-11-13 12:33:37 +0000

    Feat: Conta validate cliente id

--- src/main/java/com/banco/service/impl/ContaServiceImpl.java
+++ src/main/java/com/banco/service/impl/ContaServiceImpl.java
@@ -5,18 +5,32 @@ import java.util.List;
 import com.banco.exceptions.ContaException;
 import com.banco.exceptions.InsufficientBalanceException;
 import com.banco.exceptions.NifException;
+import com.banco.model.ClienteAbstrato;
 import com.banco.model.ContaAbstrata;
 import com.banco.model.ContaDTO;
+import com.banco.repository.ClienteDAO;
 import com.banco.repository.ContaDAO;
+import com.banco.repository.impl.ClienteDAOImpl;
 import com.banco.repository.impl.ContaDAOImpl;
 import com.banco.service.ContaService;
 
 public class ContaServiceImpl implements ContaService {
 
 	private ContaDAO repository;
+	private ClienteDAO cliente;
 
 	public ContaServiceImpl() {
 		repository = new ContaDAOImpl();
+		cliente = new ClienteDAOImpl();
+	}
+
+	private boolean clienteIdExists(String id) {
+		for (ClienteAbstrato clienteId : cliente.readAll()) {
+			if (clienteId.getId().equals(id)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -24,10 +38,15 @@ public class ContaServiceImpl implements ContaService {
 		if (contaDTO.id() == null || contaDTO.id().isBlank()) {
 			throw new NifException();
 		} else {
-			ContaAbstrata conta = FactoryConta.getConta(contaDTO);
-			create(conta);
-		}
+	        String newId = contaDTO.id();
 
+	        if (clienteIdExists(newId)) {
+	            ContaAbstrata conta = FactoryConta.getConta(contaDTO);
+	            create(conta);
+	        } else {
+	            throw new NifException();
+	        }
+	    }
 	}
 
 	@Override
@@ -74,7 +93,7 @@ public class ContaServiceImpl implements ContaService {
 
 	@Override
 	public ContaAbstrata debit(String id, double montante) throws ContaException, InsufficientBalanceException {
-		//TODO CALL debit in repository
+		// TODO CALL debit in repository
 		ContaAbstrata conta = repository.read(id);
 		if (conta == null) {
 			throw new ContaException();
@@ -90,7 +109,7 @@ public class ContaServiceImpl implements ContaService {
 		}
 
 	}
-	
+
 	public void clearRepository() {
 		repository.clearRepository();
 	}
